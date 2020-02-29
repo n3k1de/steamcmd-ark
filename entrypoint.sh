f374052461d760e558816451e436f08ec16d3532
xWilliam <44903208+xWilli4m@users.noreply.github.com>
2020-02-29 02:18:37 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,5 +23,5 @@ su steam -c "${STEAMCMDDIR}/steamcmd.sh +@sSteamCmdForcePlatformType linux +logi
 echo "---"
 
 # server start
-su steam -c "cd ${SERVERDIR}/ark/
+su steam -c "cd ${SERVERDIR}/ark/ &&
   ./ShooterGame/Binaries/Linux/ShooterGameServer ${MAP}?listen?Multihome=0.0.0.0?SessionName=${SERVERNAME}?MaxPlayers=${MAXPLAYERS}?QueryPort=${QUERYPORT}?RCONPort=${RCONPORT}?Port=${CLIENTPORT}?ServerPassword=${PASSWD}?ServerAdminPassword=${ADMINPASSWD} -server -log"
